625e2822fc2b2dd256903c485a3a4f496f30ce20
Delger Kh <delgerdelger107@gmail.com>
2026-09-06 15:07:10 +0800

    Add successful login and logout test

---
+++ tests/mytest.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('successful login and logout', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+  await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page).toHaveURL(/inventory.html/);
+  await expect(page.locator('.title')).toHaveText('Products');
+
+  await page.locator('#react-burger-menu-btn').click();
+  await page.getByText('Logout').click();
+
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+});
